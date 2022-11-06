0a111a14bd913240b96d2808d53a088d1b7f9a53
Asya <asyabareyan@gmail.com>
2022-11-06 16:11:54 +0400

    AlbumsMicroservice

--- src/main/java/com/appsdeveloperblog/photoapp/api/albums/PhotoAppApiAlbumsApplication.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/albums/PhotoAppApiAlbumsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
 public class PhotoAppApiAlbumsApplication {
 
 	public static void main(String[] args) {
